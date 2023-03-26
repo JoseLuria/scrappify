3e162b6f6ed68c42f549c71e3125c7fa991c2cf8
JoseLuria <jos02-log18@hotmail.com>
2023-03-25 22:48:45 -0500

    refactor: :art: added status type and changed status icons

--- scrapper/utils.ts
+++ scrapper/utils.ts
@@ -1,15 +1,17 @@
+import type { StatusOptions } from './types'
 import { writeFile } from 'node:fs/promises'
 import path from 'node:path'
 import * as cheerio from 'cheerio'
 import axios from 'axios'
 
-export const status = ({ success, msg }: { success: boolean; msg: string }): void => {
-  console.log(`status: ${success ? '🟢' : '🔴'} ${msg}`)
+export const statusMsg = ({ status = 'success', msg }: StatusOptions): void => {
+  const statusIcons = { success: '🟢', warn: '🟡', error: '🔴' }
+  console.log(`${status}: ${statusIcons[status]} ${msg}`)
 }
 
 export const scrape = async (URL: string): Promise<cheerio.CheerioAPI> => {
   const { data: html } = await axios.get(URL)
-  status({ success: true, msg: 'loaded cheerio API' })
+  statusMsg({ msg: 'loaded cheerio API' })
   return cheerio.load(html)
 }
 
@@ -17,5 +19,5 @@ export const saveJSON = async (JSONText: string, name: string): Promise<void> =>
   const ROOT_PATH = process.cwd()
   const filePath = path.join(ROOT_PATH, `data/${name}.json`)
   await writeFile(filePath, JSONText, 'utf-8')
-  status({ success: true, msg: `created ${name} JSON file` })
+  statusMsg({ msg: `created ${name} JSON file` })
 }

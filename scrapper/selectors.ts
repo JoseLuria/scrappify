88ac009513fa0340d7de4f3b898a4508b5fcdb50
JoseLuria <jos02-log18@hotmail.com>
2023-03-29 20:30:23 -0500

    refactor: :art: added html selector constants

---
+++ scrapper/selectors.ts
@@ -0,0 +1,5 @@
+export const SONG = '.chart-results-list .o-chart-results-list-row-container'
+
+export const SONG_TITLE = 'li:last-child ul li:first-child h3'
+
+export const SONG_ARTIST = 'li:last-child ul li:first-child span'
